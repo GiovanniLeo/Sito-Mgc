92e1dcf5cb4a3e13859f669c090b8ffc1345991a
Paolo Di Filippo <Paolo Di Filippo>
2016-09-21 18:22:44 +0200

    aggiornamento pagine con i contenuti

--- movCittadino.php
+++ movCittadino.php
@@ -4,12 +4,23 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <link rel="icon" href="images/logoPiccolo.png">
 <link type="text/css" rel="stylesheet" href="css\glob.css">
-<title>Insert title here</title>
+<link type="text/css" rel="stylesheet" href="css\inTile.css">
+<title>Movimento Giovanile per il Cambiamento</title>
 </head>
 <body>
+
 	<?php include "nav.php" ?>
-	<div class="main">
+
+	<div class ="main">
+		<p class ="year">2016</p>
+		<img src = "images/movcitt/LocadinaBenevento.jpg">
+		<p class ="year">2014</p>
+		<img src ="images/movcitt/letrai.jpg">
+		<p class = "year">2013</p>
+		<img src ="images/movcitt/DenunciaDay.jpg">
 	</div>
+
+	<?php include "footer.php" ?>
 
 </body>
 </html>
