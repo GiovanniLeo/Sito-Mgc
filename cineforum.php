e6105e52f235f525207c31e76020a3d2a16a00ac
Paolo Di Filippo <Paolo Di Filippo>
2016-09-26 19:26:49 +0200

    Aggiunta immagini

--- cineforum.php
+++ cineforum.php
@@ -13,10 +13,12 @@
 
 	<?php include "nav.php" ?>
 	<div class="main">
-		<p class="year">2015</p>
+		<p class="year">2014</p>
 		<img class="imgDim" src = "images/cineforum/north_country_xlg.jpg" alt="Locadina North Country">
 		<br>
+		<p class="year">2012</p>
 		<img class="imgDim" src = "images/cineforum/La figura_di_Giancarlo_Siani_e_gli_esempi_per_le_nuove generazioni.jpg" alt="Manifesto Giancarlo Siani">
+		<br>
 	</div>
 	<?php include "footer.php" ?>
 
